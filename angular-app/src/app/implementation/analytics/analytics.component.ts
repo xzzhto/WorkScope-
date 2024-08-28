7b6669b3f9efd9c809128a3a9c2b048cfe4a3f52
Arslan <arslanhe65@gmail.com>
2024-08-28 19:25:14 +0300

    Ваше сообщение коммита

--- angular-app/src/app/implementation/analytics/analytics.component.ts
+++ angular-app/src/app/implementation/analytics/analytics.component.ts
@@ -1,5 +1,6 @@
+// src/app/implementation/analytics/analytics.component.ts
 import { Component, OnInit } from '@angular/core';
-import * as echarts from 'echarts';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-analytics',
@@ -8,51 +9,87 @@ import * as echarts from 'echarts';
 })
 export class AnalyticsComponent implements OnInit {
   filters = {
-    category: 'all',
-    region: 'all',
-    experience: 'all'
+    language: 'Все',
+    region: 'Все',
+    experience: 'Все'
   };
 
-  jobCategoryChartOptions: any;
-  salaryChartOptions: any;
+  languages = ['Все', 'JavaScript', 'Python', 'Java', 'C#', 'PHP', 'Ruby', 'Go'];
+  regions = ['Все', 'Москва', 'Санкт-Петербург', 'Новосибирск', 'Екатеринбург'];
+  experiences = ['Все', '1-3 года', '3-5 лет', '5-10 лет', '10+ лет'];
 
-  ngOnInit() {
-    this.loadCharts();
+  showMoreCharts = false;
+
+  averageSalaryChartOptions: EChartsOption = {
+    title: { text: 'Средняя зарплата по языкам программирования' },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['JavaScript', 'Python', 'Java', 'C#', 'PHP']
+    },
+    yAxis: { type: 'value' },
+    series: [{
+      data: [120000, 110000, 115000, 105000, 95000],
+      type: 'bar',
+      itemStyle: { color: '#007bff' }
+    }]
+  };
+
+  vacancyCountChartOptions: EChartsOption = {
+    title: { text: 'Количество вакансий по месяцам' },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
+    },
+    yAxis: { type: 'value' },
+    series: [{
+      data: [200, 220, 250, 300, 270, 290, 320, 310, 280, 300, 270, 250],
+      type: 'line',
+      itemStyle: { color: '#28a745' }
+    }]
+  };
+
+  responseRateChartOptions: EChartsOption = {
+    title: { text: 'Среднее количество откликов по языкам программирования' },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['JavaScript', 'Python', 'Java', 'C#', 'PHP']
+    },
+    yAxis: { type: 'value' },
+    series: [{
+      data: [30, 40, 35, 25, 20],
+      type: 'bar',
+      itemStyle: { color: '#007bff' }
+    }]
+  };
+
+  vacancyByRegionChartOptions: EChartsOption = {
+    title: { text: 'Количество вакансий по регионам' },
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      data: ['Москва', 'Санкт-Петербург', 'Новосибирск', 'Екатеринбург']
+    },
+    yAxis: { type: 'value' },
+    series: [{
+      data: [1200, 800, 600, 400],
+      type: 'bar',
+      itemStyle: { color: '#28a745' }
+    }]
+  };
+
+  ngOnInit(): void {
+    // Инициализация графиков с базовыми данными
+    this.applyFilters();
   }
 
-  loadCharts() {
-    this.jobCategoryChartOptions = {
-      title: { text: 'Вакансии по категориям' },
-      tooltip: {},
-      xAxis: {
-        type: 'category',
-        data: ['Разработка ПО', 'Дизайн', 'Анализ данных']
-      },
-      yAxis: { type: 'value' },
-      series: [{
-        data: [100, 50, 75],
-        type: 'bar'
-      }]
-    };
-
-    this.salaryChartOptions = {
-      title: { text: 'Средняя зарплата по категориям' },
-      tooltip: {},
-      xAxis: {
-        type: 'category',
-        data: ['Разработка ПО', 'Дизайн', 'Анализ данных']
-      },
-      yAxis: { type: 'value' },
-      series: [{
-        data: [120000, 90000, 100000],
-        type: 'line'
-      }]
-    };
+  applyFilters(): void {
+    // Логика для применения фильтров и обновления графиков
   }
 
-  applyFilters() {
-    // Логика для применения фильтров и обновления данных графиков
-    console.log('Фильтры применены:', this.filters);
-    this.loadCharts(); // Здесь вы должны обновить данные графиков на основе выбранных фильтров
+  toggleCharts(): void {
+    this.showMoreCharts = !this.showMoreCharts;
   }
 }
---
+++ jwtapp/src/test/java/net/proselayte/jwtapp/JwtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.proselayte.jwtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
